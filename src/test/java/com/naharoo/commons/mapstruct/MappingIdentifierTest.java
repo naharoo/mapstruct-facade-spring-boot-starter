739a245b88b7aa07926214b744778ac4b26d1b15
naharoo <narek.harutyunyan@sfl.am>
2020-09-27 01:45:14 +0400

    Minor Polish.

--- src/test/java/com/naharoo/commons/mapstruct/MappingIdentifierTest.java
+++ src/test/java/com/naharoo/commons/mapstruct/MappingIdentifierTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SuppressWarnings("ConstantConditions")
